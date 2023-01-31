025bc3244dfbb4ae89a59513e346368b0535a530
Patrick Goral <patgoral@gmail.com>
2023-01-31 13:05:08 -0500

    mistake

--- scripts/app.js
+++ scripts/app.js
@@ -32,7 +32,7 @@ signUpForm.addEventListener('submit', (event) => {
     .then((res) => res.json())
     .then(onSignUpSuccess)
     .catch(onSignUpFailure)
-    signUp.reset()
+    signUpForm.reset()
 })
 
 signInForm.addEventListener('submit', (event) => {
