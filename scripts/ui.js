e481a4f3115d7982312babcd0f96d90835262025
Patrick Goral <patgoral@gmail.com>
2023-01-31 14:33:10 -0500

    error test

--- scripts/ui.js
+++ scripts/ui.js
@@ -98,7 +98,9 @@ const updateTicketSetter = function (event) {
 		.then((res) => {
 			onIndexTicketSuccess(res.tickets)
 		})
-        .catch(onEditTicketFailure)
+        .catch((onEditTicketFailure) => {
+            console.error(onEditTicketFailure)
+        })
         modalCloser()
         
              
